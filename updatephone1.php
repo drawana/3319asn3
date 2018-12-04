ab24bb7d65836fcd6f1728585bde551d953019c7
Daniel Rawana <drawana2@uwo.ca>
2018-12-04 03:39:38 -0500

    update phone

--- updatephone1.php
+++ updatephone1.php
@@ -7,10 +7,10 @@ $data = mysqli_fetch_assoc($result);
 if (empty($data['phonenumber'])) {
     echo "ERROR: Customer ID is invalid.";
 } else {
-    echo "Current number: ".$data['phonenumber']."\n";
+    echo "Current number: " . $data['phonenumber'] . "\n";
     $html = "<form method='post' action=''>";
     $html .= "New number: <input type=\"text\" name=\"newNumber\">";
-    $html .= "Customer ID: <input type=\"hidden\" name=\"phoneID\" value='$phoneID'>";
+    $html .= "<input type=\"hidden\" name=\"phoneID\" value='$phoneID'>";
     $html .= "<input type=\"submit\" value=\"Submit\"/>";
     $html .= "</form>";
     echo $html;
