0a41a6265c0450fe8962a44a111a2025686e330b
Daniel Rawana <drawana2@uwo.ca>
2018-12-04 03:18:06 -0500

    update phone

--- updatephone2.php
+++ updatephone2.php
@@ -1,5 +1,6 @@
 <?php
 $newNumber = $_POST['newNumber'];
+echo $phoneID;
 $sql = "UPDATE customer SET phonenumber = '$newNumber' WHERE customerID='$phoneID'";
 if (mysqli_query($connection, $sql)) {
     echo "Phone number changed successfully.";
