e346cc3799b98b5149cf14323fa8e272be4b7622
Daniel Rawana <drawana2@uwo.ca>
2018-12-04 00:39:56 -0500

    show products

--- showproducts.php
+++ showproducts.php
@@ -1,4 +1,5 @@
 <?php
+include "connecttodb.php";
 if ($_POST['alphaPrice'] == 0 and $_POST['ascDesc'] == 0)
     $query = "SELECT * FROM product ORDER BY description;";
 else if ($_POST['alphaPrice'] == 1 and $_POST['ascDesc'] == 0)
@@ -14,7 +15,7 @@ if (!$result) {
 }
 echo "<ul>";
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "<li>" . "ID: " . $row["productID"] . ", Description: " . $row["description"] . ", Price: " . $row["cost"] .", Stock: " . $row["stock"] ."</li>";
+    echo "<li>" . "ID: " . $row["productID"] . ", Description: " . $row["description"] . ", Price: " . $row["cost"] . ", Stock: " . $row["stock"] . "</li>";
 }
 echo "</ul>"; //end the bulleted list
 mysqli_free_result($result);
