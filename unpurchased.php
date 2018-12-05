5364f09dc33a9c89061a5bf2bc7cd26947defd16
Daniel Rawana <drawana2@uwo.ca>
2018-12-04 19:57:32 -0500

    unpurchased

--- unpurchased.php
+++ unpurchased.php
@@ -1,12 +1,12 @@
 <?php
-$query = "SELECT description FROM product WHERE productID NOT IN (SELECT productID FROM purchased)";
+$query = "SELECT description FROM product WHERE productID NOT IN (SELECT productID FROM purchases)";
 $result = mysqli_query($connection, $query);
 if (!$result) {
     die("databases query failed.");
 }
 echo "<ul>";
 while ($row = mysqli_fetch_assoc($result)) {
-    echo "<li>" . "Item: " . $row["description"] . "</li>";
+    echo "<li>" . $row["description"] . "</li>";
 }
 echo "</ul>"; //end the bulleted list
 mysqli_free_result($result);
